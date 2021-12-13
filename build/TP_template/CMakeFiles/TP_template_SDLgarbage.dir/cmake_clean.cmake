bccf444f306e345e0d92cadf810413062e2f3834
limserhane <dev@limserhane.fr>
2021-12-13 12:50:49 +0100

    init

---
+++ build/TP_template/CMakeFiles/TP_template_SDLgarbage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_template_SDLgarbage.dir/SDLgarbage.cpp.o"
+  "TP_template_SDLgarbage"
+  "TP_template_SDLgarbage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_template_SDLgarbage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
